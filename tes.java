7bfe99e3cad662a1f11dd0df678398855811bf61
HAMSAH107 <79818784+HAMSAH107@users.noreply.github.com>
2025-03-08 09:08:39 +0800

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,10 @@
+import javax.xml.crypto.Data;
+
+public class tes {
+    public static void main(String[] args) {
+        database db = new database();
+        db.open();
+        db.view();
+    }
+
+}
